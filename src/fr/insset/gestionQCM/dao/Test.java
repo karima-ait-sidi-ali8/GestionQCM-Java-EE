674f6ee482180fa0dba5c9c79bac2d833682b09c
Mossaab Frifita <frifita1@gmail.com>
2018-05-15 12:43:04 +0200

    Add QcmAuteur managed Bean & List qcms for each auteur

--- src/fr/insset/gestionQCM/dao/Test.java
+++ src/fr/insset/gestionQCM/dao/Test.java
@@ -7,12 +7,15 @@ package fr.insset.gestionQCM.dao;
 import org.apache.log4j.Logger;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import fr.insset.gestionQCM.dao.entity.Auteur;
 import fr.insset.gestionQCM.dao.entity.Etudiant;
 import fr.insset.gestionQCM.dao.entity.Groupe;
+import fr.insset.gestionQCM.dao.entity.Qcm;
 import fr.insset.gestionQCM.dao.entity.Utilisateur;
 import fr.insset.gestionQCM.metier.GroupeMetier;
 import fr.insset.gestionQCM.metier.QcmMetier;
 import fr.insset.gestionQCM.metier.UserMetier;
+import fr.insset.gestionQCM.utils.ContextUtil;
 
 
 
@@ -21,15 +24,15 @@ public class Test {
 	public static Logger log = Logger.getLogger(Test.class);
 	public static void main(String[] args) {
 		
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"config/config.xml"});
 		
+		UserMetier metier = (UserMetier) ContextUtil.getContext().getBean("metier"); 
+		ContextUtil.getContext().close();
+		
+		Auteur auteur = (Auteur) metier.finByOne(new Integer(1));
+	
+		Qcm q  = auteur.getQcms().get(0);
 
-		UserMetier metier = (UserMetier) context.getBean("metier"); 
-		
-		context.close();
-		
-
-		
+		System.out.println(q.getTitre());
 
 	}
 
---
+++ src/fr/insset/gestionQCM/presentation/QcmAuteurBean.java
@@ -0,0 +1,88 @@
+package fr.insset.gestionQCM.presentation;
+
+
+
+import java.io.Serializable;
+import java.util.List;
+
+
+
+import javax.annotation.PostConstruct;
+
+import javax.faces.bean.ManagedBean;
+
+
+import javax.faces.bean.SessionScoped;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+
+import fr.insset.gestionQCM.dao.entity.Auteur;
+
+import fr.insset.gestionQCM.dao.entity.Qcm;
+
+import fr.insset.gestionQCM.metier.UserMetier;
+import fr.insset.gestionQCM.utils.ContextUtil;
+import fr.insset.gestionQCM.utils.SessionUtil;
+
+
+
+
+@ManagedBean(name="etudiants")
+@SessionScoped
+public class QcmAuteurBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	public Logger log = Logger.getLogger(UserBean.class);
+
+	
+	private List<Qcm> ListeQcms;
+
+
+	
+	public QcmAuteurBean() {
+		super();
+	}
+
+
+	 
+	@PostConstruct
+	public void initBean(){
+
+
+		HttpSession hs = SessionUtil.getSession();
+		
+		UserMetier metier = (UserMetier) ContextUtil.getContext().getBean("metier"); 
+		ContextUtil.getContext().close();
+		
+		Auteur auteur = (Auteur) metier.finByOne((Integer) hs.getAttribute("idUser"));
+	
+		ListeQcms = auteur.getQcms();
+		
+		
+
+	}
+
+
+
+	public List<Qcm> getListeQcms() {
+		return ListeQcms;
+	}
+
+
+
+	public void setListeQcms(List<Qcm> listeQcms) {
+		ListeQcms = listeQcms;
+	}
+	
+
+
+}
+
+
+
+
+
+
+
